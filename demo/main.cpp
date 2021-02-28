a48ed835f0e7cd6ff64c101352e028cd7d42d07f
steven <steven0909509363@gmail.com>
2021-02-28 10:02:58 +0800

    new main.cpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -5,17 +5,16 @@ DigitalOut myLED2(LED1);
 
 int main()
 {
-   myLED = 1;
-   for (int i = 0; i < 6; ++i)
-   {                     //blink for 6 times
-      myLED = !myLED; // toggle led
-      ThisThread::sleep_for(1000ms);
+   while (1) {
+      myLED = 1;
+      for (int i = 0; i < 6; ++i) {                     //blink for 6 times
+         myLED = !myLED; // toggle led
+         ThisThread::sleep_for(100ms);
+      }
+      myLED2 = 1;
+      for (int i = 0; i < 4; ++i) {                     //blink for 6 times
+         myLED2 = !myLED2; // toggle led
+         ThisThread::sleep_for(100ms);
+      }
    }
-   myLED2 = 1;
-   for (int i = 0; i < 4; ++i)
-   {                     //blink for 6 times
-      myLED2 = !myLED2; // toggle led
-      ThisThread::sleep_for(1000ms);
-   }
-   myLED2 = 0;
 }
